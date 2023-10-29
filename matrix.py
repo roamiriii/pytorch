39314e74e957fae6631d37981bd301842b572c44
roamiriii <149173245+roamiriii@users.noreply.github.com>
2023-10-29 11:17:42 +0330

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+# Define functions related to matrices
+def create_matrix(rows, cols):
+    """Create a matrix of custom size."""
+    return np.zeros((rows, cols))
+
+def add_matrices(matrix1, matrix2):
+    """Add two matrices."""
+    return np.add(matrix1, matrix2)
+
+def subtract_matrices(matrix1, matrix2):
+    """Subtract two matrices."""
+    return np.subtract(matrix1, matrix2)
+
+def multiply_matrices(matrix1, matrix2):
+    """Multiply two matrices."""
+    return np.dot(matrix1, matrix2)
+
+def transpose_matrix(matrix):
+    """Transpose a matrix."""
+    return np.transpose(matrix)
+
+# Example using the functions
+matrix_a = create_matrix(2, 3)
+matrix_b = create_matrix(3, 2)
+
+matrix_a[0][0] = 1
+matrix_a[0][1] = 2
+matrix_a[0][2] = 3
+matrix_a[1][0] = 4
+matrix_a[1][1] = 5
+matrix_a[1][2] = 6
+
+matrix_b[0][0] = 7
+matrix_b[0][1] = 8
+matrix_b[1][0] = 9
+matrix_b[1][1] = 10
+matrix_b[2][0] = 11
+matrix_b[2][1] = 12
+
+result_add = add_matrices(matrix_a, matrix_b)
+result_subtract = subtract_matrices(matrix_a, matrix_b)
+result_multiply = multiply_matrices(matrix_a, matrix_b)
+result_transpose = transpose_matrix(matrix_a)
+
+print("Matrix addition:")
+print(result_add)
+
+print("Matrix subtraction:")
+print(result_subtract)
+
+print("Matrix multiplication:")
+print(result_multiply)
+
+print("Transpose of a matrix:")
+print(result_transpose)
